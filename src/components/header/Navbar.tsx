56c41ca32e89abcaeab8b4c3d2f211f1f0dd9860
winaatee <wisuda.dc@gmail.com>
2024-08-14 20:25:12 +0700

    fix: hide logo

--- src/components/header/Navbar.tsx
+++ src/components/header/Navbar.tsx
@@ -10,7 +10,7 @@ export default function Navbar() {
                     <div className="flex lg:flex-1">
                         <a href="#" className="-m-1.5 p-1.5">
                             <span className="sr-only">Wiwi</span>
-                            <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+                            {/* <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" /> */}
                         </a>
                     </div>
                     <div className="hidden lg:flex lg:flex-1 lg:justify-end gap-x-12">
